ce191cb53ada4b82768bd35898e1ac78a0faa14c
simon.paris <simon.paris@blade-group.com>
2021-07-28 12:59:57 +0200

    exec move

--- src/solitaire.py
+++ src/solitaire.py
@@ -1,15 +1,6 @@
 import matplotlib.pyplot as plt
+import random
 
-
-def display(p_board):
-	for k, v in p_board.items():
-		if v == 1:
-			plt.scatter(k[0], k[1], color='red')
-		else:
-			plt.scatter(k[0], k[1], color='white')
-
-	plt.axis([-1, 7, -1, 7])
-	plt.show()
 
 def init_board():
 	board = dict()
@@ -28,6 +19,46 @@ def init_board():
 
 	return board
 
+def display(p_board):
+	for k, v in p_board.items():
+		if v == 1:
+			plt.scatter(k[0], k[1], color='red')
+		else:
+			plt.scatter(k[0], k[1], color='white')
+
+	plt.axis([-1, 7, -1, 7])
+	plt.show()
+
+def find_moves(p_board):
+	moves = []
+	direction =[(0, 1), (1, 0), (0, -1), (-1, 0)]
+	for k, v in p_board.items():
+		if v == 0:
+			continue
+		else:
+			for i in direction:
+				slot = p_board.get((int(k[0] + i[0]), int(k[1]) + i[1]))
+				if slot is not None and slot == 1:
+					slot = p_board.get((k[0] + i[0] * 2, k[1] + i[1] * 2))
+					if slot is not None and slot == 0:
+						moves.append((k, (k[0] + i[0] * 2, k[1] + i[1] * 2)))
+	return moves
+
+def exec_move(p_board, move):
+	p_board[move[0]] = 0
+	p_board[move[1]] = 1
+	if move[0][0] > move[1][0]:
+		p_board[(move[0][0] -1, move[0][1])] = 0
+	elif move[0][0] < move[1][0]:
+		p_board[(move[0][0] + 1, move[0][1])] = 0
+	else:
+		if move[0][1] > move[1][1]:
+			p_board[(move[0][0], move[0][1] - 1)] = 0
+		elif move[0][1] < move[1][1]:
+			p_board[(move[0][0], move[0][1] + 1)] = 0
+		else:
+			print("ERROR")
+	return p_board
 
 # init
 
@@ -39,19 +70,9 @@ board[(3, 3)] = 0
 
 # rounds
 
-moves = []
-direction =[(0, 1), (1, 0), (0, -1), (-1, 0)]
-for k, v in board.items():
-	if v == 0:
-		continue
-	else:
-		for i in direction:
-			slot = board.get((int(k[0] + i[0]), int(k[1]) + i[1]))
-			if slot is not None and slot == 1:
-				slot = board.get((k[0] + i[0] * 2, k[1] + i[1] * 2))
-				if slot is not None and slot == 0:
-					moves.append((k, (k[0] + i[0] * 2, k[1] + i[1] * 2)))
-
-print(moves)
+moves = find_moves(board)
+rand = random.randint(0, len(moves) - 1)
+board = exec_move(board, moves[rand])
+display(board)
 
 
